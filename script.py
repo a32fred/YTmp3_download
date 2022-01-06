d174c7aba42b0f18a5c6a1e33981783721792217
a32fred <a.32.freeed@gmail.com>
2022-01-06 20:17:05 -0300

    inputs traduzidos para o portugues

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import youtube_dl
 def run():
-    video_url = input("please enter youtube video url:")
+    video_url = input("pfv coloque o link do seu video aqui:")
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
     filename = f"{video_info['title']}.mp3"
     options={
@@ -12,7 +12,7 @@ def run():
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
-    print("Download complete... {}".format(filename))
+    print("Download Finalozado!!! {}".format(filename))
 
 if __name__=='__main__':
     run()
